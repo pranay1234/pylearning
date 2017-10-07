0c102e8caef1c38f417b3a918c0e751067996a89
Aluru <alooripr@gmail.com>
2017-10-07 04:39:07 -0400

    updated test program with correct type casting

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 months = ["jan", "feb", "mar","apirl","may","june","july","Aug","sep","oct","nov","dec"]
 endings = ['st','nd','rd'] + 17*['th']+['st']+['nd']+['rd']+7*['th']+['st']
-year = input('year is:')
-month = input ('month is:')
-day = input ('day is :')
-months_ned = month-1
+year = input('Year: ')
+month = input('Month (1-12): ')
+day = input('Day (1-31): ')
+month_number = int(month)
+day_number = int(day)
+month_name = months[month_number-1]
+ordinal = str(day_number) +  endings[day_number-1]
+print (month_name + ' ' + ordinal + ', ' + str(year))
+print "Test"
 
-
-if len(months) < months_ned:
-    print "Range is out of months listed"
-    quit()
-print (str(day)+(endings[day-1]), months_ned,months[months_ned])
